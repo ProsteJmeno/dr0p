58efca5d0dae035820fcff3972c8ec619fc88dfe
ProsteJmeno <mujucetproreklamy@gmail.com>
2021-03-31 17:29:03 +0200

    Přidaný package pro play services a AndroidManifest.xml
    Naimportovaný package třetí strany, jenž by měl umožnit propojení projektu s Play Services (zatím se to moc nezdá), a přidán AndroidManifest.xml soubor, stažený ze samplu googlu

---
+++ Assets/scripts/PlayServices.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
+
+public class PlayServices : MonoBehaviour
+{
+    public int playerScore;
+    string leaderboardID = "CgkIoJmE3YUXEAIQAA";
+
+
+    void Start()
+    {
+
+        DontDestroyOnLoad(this);
+        try
+        {
+            PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+            PlayGamesPlatform.InitializeInstance(config);
+            PlayGamesPlatform.DebugLogEnabled = true;
+            PlayGamesPlatform.Activate();
+            Social.localUser.Authenticate((bool success) => { });
+        }
+        catch (Exception exception)
+        {
+            Debug.Log(exception);
+        }
+    }
+
+    public void AddScoreToLeaderboard()
+    {
+        if (Social.localUser.authenticated)
+        {
+            Social.ReportScore(playerScore, leaderboardID, success => { });
+        }
+    }
+
+    public void ShowLeaderboard()
+    {
+        print("debug");
+        if (Social.localUser.authenticated)
+        {
+            Social.ShowLeaderboardUI();
+            print("leaderboard");
+        }
+        Social.ShowLeaderboardUI();
+    }
+}
